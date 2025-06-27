54426cf28fc62b079f30eee80e142ba08b442508
= <=>
2025-06-27 20:02:54 +0200

    drawing a random image

--- fractol.h
+++ fractol.h
@@ -6,23 +6,41 @@
 /*   By: marcos <marcos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 13:14:16 by marcos            #+#    #+#             */
-/*   Updated: 2025/06/26 20:29:30 by marcos           ###   ########.fr       */
+/*   Updated: 2025/06/27 19:45:12 by marcos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+typedef	struct	xWindow
+{
+	void	*win;
+	int		heigth;
+	int		width;
+}	s_xWindow;
+
+typedef	struct	xImage
+{
+	void	*img;
+	int		heigth;
+	int		width;
+	int		bpp;
+	int		sl;
+	int		end;
+}	s_xImage;
+
 typedef struct	xScreen
 {
-	void	*mlx;
-	void	*win;
-	void	*img;
+	void		*mlx;
+	s_xWindow	*xWin;
+	s_xImage	*xImg;
 }	s_xScreen;
 
 # include "mlx.h"
 # include "mlx_int.h"
 # include "libft/libft.h"
 # include "libft/libftprintf/libftprintf.h"
+# include "libft/libftget_next_line/libftget_next_line.h"
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: marcos <marcos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 13:13:14 by marcos            #+#    #+#             */
-/*   Updated: 2025/06/26 21:18:55 by marcos           ###   ########.fr       */
+/*   Updated: 2025/06/27 20:01:53 by marcos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,35 @@
 #include <stdio.h>
 #include <math.h>
 
+s_xWindow	*createWindow(s_xScreen *xScreen, int width, int heigth)
+{
+	s_xWindow 	*xWin;
+
+	xWin = ft_calloc(sizeof(s_xWindow *), 1);
+	xWin->width = width;
+	xWin->heigth = heigth;
+	if(!(xWin->win = mlx_new_window(xScreen->mlx, xWin->heigth, xWin->width, "Window title")))
+		return (NULL);
+	return(xWin);
+}
+
+s_xImage	*createImage(s_xScreen *xScreen, int width, int heigth)
+{
+	s_xImage 	*xImg;
+
+	xImg = ft_calloc(sizeof(s_xImage *), 1);
+	xImg->width = width;
+	xImg->heigth = heigth;
+	if(!(xImg->img = mlx_new_image(xScreen->mlx, xImg->heigth, xImg->width)))
+		return (NULL);
+	return(xImg);
+}
+
 int	closeWindow(s_xScreen *xScreen)
 {
 	printf("Closing...\n");
-	mlx_clear_window(xScreen->mlx, xScreen->win);
-	mlx_destroy_window(xScreen->mlx, xScreen->win);
+	mlx_clear_window(xScreen->mlx, xScreen->xWin->win);
+	mlx_destroy_window(xScreen->mlx, xScreen->xWin->win);
 	exit (0);
 }
 
@@ -29,18 +53,40 @@ int keyEvent(int key, s_xScreen *xScreen)
 	return (1);
 }
 
-void	drawWindow(s_xScreen *xScreen)
+int	chooseColor(int x, int width, int y, int heigth)
 {
-	int	x = -200;
-	int y;
+	int	r;
+	int	g;
+	int	b;
 
-	while (x <= 200)
+	r = ((x * y) * 256 / width);
+	g = (x * 256 / heigth) * pow(16, 2);
+	b = (x * 256 / width) * pow(16, 4);
+	return (r + g + b);
+}
+
+void	drawImage(s_xScreen *xScreen, int width, int heigth)
+{
+	int					x;
+	int					y;
+	int					color;
+	unsigned int		imgColor;
+	char				*imgPositon;
+
+	imgPositon = mlx_get_data_addr(xScreen->xImg->img, &(xScreen->xImg->bpp), &(xScreen->xImg->sl), &(xScreen->xImg->end));
+	y = 0;
+	while (y < heigth)
 	{
-		y = 3 + sqrt(10000 - pow((x - 2), 2));
-		mlx_pixel_put(xScreen->mlx, xScreen->win, x + 200, y + 200, 0xFFFFFF);
-		y = 3 - sqrt(10000 - pow((x - 2), 2));
-		mlx_pixel_put(xScreen->mlx, xScreen->win, x + 200, y + 200, 0xFFFFFF);
-		x++;
+		x = 0;
+		while (x < width)
+		{
+			color = chooseColor(x, width, y, heigth);
+			imgColor = mlx_get_color_value(xScreen->mlx, color);
+			*(unsigned int *)imgPositon = imgColor;
+			x++;
+			imgPositon += xScreen->xImg->bpp / 8;
+		}
+		y++;
 	}
 }
 
@@ -51,13 +97,15 @@ int	main()
 	xScreen = ft_calloc(sizeof(s_xScreen *), 1);
 	if (!(xScreen->mlx = mlx_init()))
 		exit(1);
-	if (!(xScreen->win = mlx_new_window(xScreen->mlx, 500, 500, "Window title")))
+	if (!(xScreen->xWin = createWindow(xScreen, 500, 500)))
 		exit(1);
-
-	drawWindow(xScreen);
+	if (!(xScreen->xImg = createImage(xScreen, 250, 250)))
+		exit(1);
+	drawImage(xScreen, xScreen->xImg->width, xScreen->xImg->heigth);
+	mlx_put_image_to_window(xScreen->mlx, xScreen->xWin->win, xScreen->xImg->img, 0, 0);
 	//manage the window close event
-	mlx_hook(xScreen->win, 17, 1L << 1, closeWindow, xScreen);
-	mlx_hook(xScreen->win, 2, 1L << 0, keyEvent, xScreen);
+	mlx_hook(xScreen->xWin->win, 17, 1L << 1, closeWindow, xScreen);
+	mlx_hook(xScreen->xWin->win, 2, 1L << 0, keyEvent, xScreen);
 	//function needed to listen to events
 	mlx_loop(xScreen->mlx);
 }
